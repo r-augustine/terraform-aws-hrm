8ada0796a240ff3ac851d883800605aa44121624
Ricardo <augustineri93@gmail.com>
2022-12-26 21:49:57 -0400

    fix(amplify): update artifact base directory

--- amplify.tf
+++ amplify.tf
@@ -1,9 +1,9 @@
 resource "aws_amplify_app" "hrm-app" {
-  name       = "HRM Application"
-  repository = aws_codecommit_repository.hrm.clone_url_http
-  iam_service_role_arn = aws_iam_role.amplify-codecommit.arn
+  name                     = "HRM Application"
+  repository               = aws_codecommit_repository.hrm.clone_url_http
+  iam_service_role_arn     = aws_iam_role.amplify-codecommit.arn
   enable_branch_auto_build = true
-  build_spec = <<-EOT
+  build_spec               = <<-EOT
     version: 0.1
     frontend:
       phases:
@@ -14,7 +14,7 @@ resource "aws_amplify_app" "hrm-app" {
           commands:
             - npm run build
       artifacts:
-        baseDirectory: build
+        baseDirectory: dist
         files:
           - '**/*'
       cache:
@@ -35,13 +35,13 @@ resource "aws_amplify_app" "hrm-app" {
 resource "aws_amplify_branch" "dev" {
   app_id      = aws_amplify_app.hrm-app.id
   branch_name = "dev"
-  framework = "React"
-  stage     = "DEVELOPMENT"
+  framework   = "React"
+  stage       = "DEVELOPMENT"
 }
 
 resource "aws_amplify_branch" "main" {
   app_id      = aws_amplify_app.hrm-app.id
   branch_name = "main"
-  framework = "React"
-  stage     = "PRODUCTION"
+  framework   = "React"
+  stage       = "PRODUCTION"
 }
